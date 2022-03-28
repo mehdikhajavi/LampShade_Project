168cb899a015c695931e2a57f60b74accfca8156
mohammad mahdi khajavi <mehdiikhajavi@gmail.com>
2022-03-28 19:54:33 +0330

    initialize ServiceHost

--- LampShade/ShopManagement.Configuration/ShopManagementBootstraper.cs
+++ LampShade/ShopManagement.Configuration/ShopManagementBootstraper.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ShopManagement.Application;
 using ShopManagement.Application.Contracts.ProductCategory;
 using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore;
 using ShopManagement.Infrastructure.EFCore.Repositories;
 using System;
 
@@ -9,10 +11,14 @@ namespace ShopManagement.Configuration
 {
     public class ShopManagementBootstraper
     {
-        public static void Configure(IServiceCollection services , string connectionString)
+        public static void Configure(IServiceCollection services , string ConnectionString)
         {
             services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
-            services.AddTransient<IProductCategoryApplication , ProductCategoryApplication>
+            services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+
+
+
+            services.AddDbContext<ShopContext>(x => x.UseSqlServer(ConnectionString));
         }
     }
 }
